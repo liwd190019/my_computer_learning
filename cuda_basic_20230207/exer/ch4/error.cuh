afcd2ec835fc9365724ccc6da79c68159caa7ceb
wenda_li <liwd190019@sjtu.edu.cn>
2023-02-10 11:23:39 +0800

    20230210

---
+++ cuda_basic_20230207/exer/ch4/error.cuh
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdio.h>
+
+#define CHECK(call)                                                         \
+do                                                                          \
+{                                                                           \
+    const cudaError_t error_code = call;                                    \
+    if (error_code != cudaSuccess){                                         \
+        printf("CUDA Error:\n");                                            \
+        printf("    File:     %s\n", __FILE__);                             \
+        printf("    Line:     %d\n", __LINE__);                             \
+        printf("    Error code: %d\n", error_code);                         \
+        printf("    Error text: %s\n", cudaGetErrorString(error_code));     \
+        exit(1);                                                            \
+    }                                                                       \
+} while (0);
+
+/**
+ * why do we use do-while statement here?
+ * 由于在某些情况下不安全
+*/
